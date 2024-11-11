70bca4f083bda10f70db888746f50889c2cb33ad
Jaison John <jaison.john@ileafsolutions.com>
2024-11-11 18:29:26 +0530

    cleanup

--- extension/background.js
+++ extension/background.js
@@ -136,51 +136,57 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "popupOpened") {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const tab = tabs[0];
-      chrome.cookies.get(
-        {
-          url: "https://portal.qa.scrambleid.com",
-          name: "scramble-session-dem",
-        },
-        async (cookie) => {
-          if (cookie && tab && tab.url === "https://demoguest.com/vdi") {
-            await chrome.scripting.executeScript({
-              target: { tabId: tab.id },
-              files: ["content.js"],
-            });
 
-            chrome.runtime.sendMessage({ action: "show_loader" });
-            await fetch(
-              "https://qa.scrambleid.com/api/v1/lid/start-session/ZGVtfHxsZGFwYXBwMQ",
-              {
-                method: "post",
-                credentials: "include",
-              }
-            )
-              .then((response) => response.json())
-              .then(async (data) => {
-                await chrome.scripting.executeScript({
-                  target: { tabId: tab.id },
-                  files: ["content.js"],
-                });
-                await chrome.tabs.sendMessage(tab.id, {
-                  action: "retrieve-user",
-                  user: data.user,
-                });
-                chrome.runtime.sendMessage({ action: "hide_loader" });
-              })
-              .catch((error) => {
-                console.error("Error fetching data:", error);
+      if (!tab || tab.url !== "https://demoguest.com/vdi") {
+        chrome.runtime.sendMessage({ action: "hide_loader" });
+        return
+      }
+
+        chrome.cookies.get(
+          {
+            url: "https://portal.qa.scrambleid.com",
+            name: "scramble-session-dem",
+          },
+          async (cookie) => {
+            if (cookie) {
+              await chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                files: ["content.js"],
               });
-          } else {
-            await chrome.scripting.executeScript({
-              target: { tabId: tab.id },
-              files: ["content.js"],
-            });
 
-            chrome.runtime.sendMessage({ action: "show_error" });
+              chrome.runtime.sendMessage({ action: "show_loader" });
+              await fetch(
+                "https://qa.scrambleid.com/api/v1/lid/start-session/ZGVtfHxsZGFwYXBwMQ",
+                {
+                  method: "post",
+                  credentials: "include",
+                }
+              )
+                .then((response) => response.json())
+                .then(async (data) => {
+                  await chrome.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    files: ["content.js"],
+                  });
+                  await chrome.tabs.sendMessage(tab.id, {
+                    action: "retrieve-user",
+                    user: data.user,
+                  });
+                  chrome.runtime.sendMessage({ action: "hide_loader_and_close_popup" });
+                })
+                .catch((error) => {
+                  console.error("Error fetching data:", error);
+                });
+            } else {
+              await chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                files: ["content.js"],
+              });
+
+              chrome.runtime.sendMessage({ action: "show_error" });
+            }
           }
-        }
-      );
+        );
     });
   }
 });
--- extension/popup.js
+++ extension/popup.js
@@ -8,16 +8,18 @@ chrome.cookies.get(
       const openButton = document.getElementById("openNewTabBtn");
       openButton.remove();
     } else {
-      document.addEventListener("DOMContentLoaded", () => {
-        const openButton = document.getElementById("openNewTabBtn");
-        openButton.addEventListener("click", () => {
-          chrome.runtime.sendMessage({ action: "openWebsite" });
-        });
-      });
       console.log("No scrambled cookie found!!");
     }
   }
 );
+
+document.addEventListener("DOMContentLoaded", () => {
+  const openButton = document.getElementById("openNewTabBtn");
+  if (!openButton) return;
+  openButton.addEventListener("click", () => {
+    chrome.runtime.sendMessage({ action: "openWebsite" });
+  });
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   chrome.runtime.sendMessage({ action: "popupOpened" });
@@ -26,10 +28,13 @@ document.addEventListener("DOMContentLoaded", () => {
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "show_loader") {
     document.getElementById("loader").style.display = "block";
-  } else if (request.action === "hide_loader") {
+  } else if (request.action === "hide_loader_and_close_popup") {
     document.getElementById("loader").style.display = "none";
     window.close();
   } else if (request.action === "show_error") {
+    document.getElementById("error-message").textContent = request.message;
+    document.getElementById("loader").style.display = "none";
+  }else if (request.action === "hide_loader") {
     document.getElementById("error-message").textContent = request.message;
     document.getElementById("loader").style.display = "none";
   }
